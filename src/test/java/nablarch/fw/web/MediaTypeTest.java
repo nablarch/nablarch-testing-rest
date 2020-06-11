8f3888de5f76dea020257ea69f6e9137cefd2d23
kumano_goro <kumano.goro@tis.co.jp>
2020-06-11 19:58:50 +0900

    JavaDoc追記

--- src/test/java/nablarch/fw/web/MediaTypeTest.java
+++ src/test/java/nablarch/fw/web/MediaTypeTest.java
@@ -9,6 +9,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * {@link MediaType}のテストクラス
+ */
 public class MediaTypeTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -23,6 +26,9 @@ public class MediaTypeTest {
     private static final String CHARSET = "charset=UTF8";
     private static final String BOUNDARY = "boundary=aBoundary";
 
+    /**
+     * Nullを引数とした場合、コンストラクタで例外が送出される。
+     */
     @Test
     public void testConstructorNullArgShouldThrowException() {
         expectedException.expect(IllegalArgumentException.class);
@@ -30,6 +36,9 @@ public class MediaTypeTest {
         new MediaType(null);
     }
 
+    /**
+     * 空文字を引数とした場合、コンストラクタで例外が送出される。
+     */
     @Test
     public void testConstructorEmptyArgShouldThrowException() {
         expectedException.expect(IllegalArgumentException.class);
@@ -37,6 +46,9 @@ public class MediaTypeTest {
         new MediaType("");
     }
 
+    /**
+     * スペースを引数とした場合、コンストラクタで例外が送出される。
+     */
     @Test
     public void testConstructorEmptyMediaTypeArgShouldThrowException() {
         expectedException.expect(IllegalArgumentException.class);
@@ -44,6 +56,9 @@ public class MediaTypeTest {
         new MediaType(" ");
     }
 
+    /**
+     * MIMEタイプにあたる部分がないContent-Typeを引数とした場合、コンストラクタで例外が送出される。
+     */
     @Test
     public void testConstructorEmptyMediaTypeWithCharsetArgShouldThrowException() {
         expectedException.expect(IllegalArgumentException.class);
@@ -51,6 +66,12 @@ public class MediaTypeTest {
         new MediaType(";" + CHARSET);
     }
 
+    /**
+     * 小文字のContent-Typeから生成した{@link MediaType}が
+     * 同じContent-Type、大文字のContent-Type、
+     * スペースが含まれているContent-Typeのそれぞれから生成した{@link MediaType}と
+     * 同値として評価されることを確認する。
+     */
     @Test
     public void testPlainContent() {
         MediaType sut = new MediaType(TEXT_PLAIN);
@@ -59,26 +80,30 @@ public class MediaTypeTest {
         assertEquals(TEXT_PLAIN_WITH_SPACE_TYPE, sut);
     }
 
-    @Test
-    public void testPlainContentUpperCase() {
-        MediaType sut = new MediaType(TEXT_PLAIN_UPPER_CASE);
-        assertEquals(TEXT_PLAIN_TYPE, sut);
-        assertEquals(TEXT_PLAIN_UPPER_CASE_TYPE, sut);
-        assertEquals(TEXT_PLAIN_WITH_SPACE_TYPE, sut);
-    }
-
+    /**
+     * charset付きのContent-Typeから想定通りの{@link MediaType}が
+     * 生成されることを確認する。
+     */
     @Test
     public void testPlainContentWithCharset() {
         MediaType sut = new MediaType(TEXT_PLAIN + ";" + CHARSET);
         assertEquals(TEXT_PLAIN_TYPE, sut);
     }
 
+    /**
+     * boundary付きのContent-Typeから想定通りの{@link MediaType}が
+     * 生成されることを確認する。
+     */
     @Test
     public void testMultipartWithBoundary() {
         MediaType sut = new MediaType(MULTIPART + ";" + BOUNDARY);
         assertEquals(MULTIPART_TYPE, sut);
     }
 
+    /**
+     * {@link MediaType#equals(Object)}、{@link MediaType#hashCode()}が
+     * 一般契約に従っていることを確認する。
+     */
     @Test
     public void testOverriddenEqualsAndHashCode() {
         MediaType x = new MediaType("content_type");
