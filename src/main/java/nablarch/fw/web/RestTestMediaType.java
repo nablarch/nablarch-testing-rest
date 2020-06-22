45724605f311744797b93a9a06d41dbb615dc862
kumano_goro <kumano.goro@tis.co.jp>
2020-06-22 11:34:47 +0900

    RestTestMediaTypeをパッケージプライベートに変更

--- src/main/java/nablarch/fw/web/RestTestMediaType.java
+++ src/main/java/nablarch/fw/web/RestTestMediaType.java
@@ -5,7 +5,7 @@ import nablarch.core.util.StringUtil;
 /**
  * MIMEタイプを表すクラス
  */
-public class RestTestMediaType {
+class RestTestMediaType {
     /** MIMEタイプ */
     private final String mediaType;
 
@@ -15,7 +15,7 @@ public class RestTestMediaType {
      *
      * @param contentType Content-Typeヘッダーの値
      */
-    public RestTestMediaType(String contentType) {
+    RestTestMediaType(String contentType) {
         if (StringUtil.isNullOrEmpty(contentType)) {
             throw new IllegalArgumentException("content type must not be empty.");
         }
